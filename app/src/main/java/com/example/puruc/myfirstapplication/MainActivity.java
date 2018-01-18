194325d6ee68f168c3a21e22f42e928b076afc40
Puru Chaudhary <puru.chaudhary17@hotmail.com>
2018-01-18 01:52:41 -0800

    Initial commit

---
+++ app/src/main/java/com/example/puruc/myfirstapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.puruc.myfirstapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+//        fab.setOnClickListener((View view) ->
+//                Snackbar.make(view, "this is my own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Settings", new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                //makes toast on fab click
+//                                Toast.makeText(getApplicationContext(), R.string.snackbar_press_action, Toast.LENGTH_SHORT).show();
+//
+//                                //opens SettingsActivity when snackbar action is clicked
+//                                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+//                                startActivity(intent);
+//                            }
+//                        }).show());
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.action_help) {
+            Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
